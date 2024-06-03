7874c30403dd45f5648920ab7fd6008df10fd6ba
zartwilly <zartwilly@gmail.com>
2024-06-03 10:02:22 +0200

    fct_aux: definitions of common functions of a game.

---
+++ auxiliary_functions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon june 3 11:03:54 2024
+
+@author: willy
+
+auxiliary_functions file contains all functions that are used to all project files.
+"""
+
+def apv(x): # Return absolute positive value 
+    return max(0,x)  
+    
+def phiepoplus(x): # Parameter :  benefit function of selling energy to EPO
+    return x * 15
+
+def phiepominus(x): # Parameter :  cost function of buying energy from EPO 
+    return x * 90
+
