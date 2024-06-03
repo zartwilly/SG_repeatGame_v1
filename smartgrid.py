be43bdde64fbe030ea563f2a9fe516e3ca50ec68
zartwilly <zartwilly@gmail.com>
2024-06-03 11:01:17 +0200

    SG: initialization of variables.

---
+++ smartgrid.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 10:07:22 2024
+
+@author: willy
+
+smartgrid_rg is the smartgrid in the repeat game that centralizes the parameters of the environment
+"""
+import math
+import random as rdm
+import numpy as np
+import agents as ag
+import auxiliary_functions as aux
+
+class Smartgrid :
+    
+    # This class represent a smartgrid
+    maxperiod = None # Number of periods
+    prosumers = None # All prosumers inside the smartgrid
+    LCostmax = None # Maximum benefit for each prosumer for each period
+    LCostmin = None # Minimum benefit for each prosumer for each period
+    insg = None # Sum of electricity input from all prosumers
+    outsg = None # Sum of electricity outputs from all prosumers
+    ValEgoc = None # sum of all valOne 
+    ValNoSG = None
+    ValSG = None
+    Reduct = None
+    strategy_profile = None 
+    Cost = None
+    DispSG = None
+    
+    
+    # TODO to delete
+    # piepoplus = None # Unitary price of electricity purchased by EPO
+    # piepominus = None # Unitary price of electricity sold by EPO
+    # piplus = None # Unitary benefit of electricity sold to SG (independently from EPO)
+    # piminus = None # Unitary cost of electricity bought from SG (independently from EPO)
+    # unitaryben = None # Unitary benefit of electricity sold to SG (possibly partially to EPO)
+    # unitarycost = None # Unitary cost of electricity bought from SG (possibly partially from EPO)
+    # betaplus = None # Intermediate values for computing piplus and real benefit 
+    # betaminus = None # Intermediate values for computing piminus and real cost
+    # czerom = None # Upper bound a prosumer could have to pay
+    # realprod = None # Real value of production for each prosumers (different from the predicted production)
+    # realstate = None # Real state of each prosumers when using real production value (can be the same as the one determined with predicted production)
+    
+    
+    def __init__(self, N, maxperiod, initialprob):
+        """
+        N = number of prosumers, 
+        maxperiod = max numbers of periods
+        initialprob : initial value of probabilities for LRI, 
+        
+        """
+        self.prosumers = np.ndarray(shape=(N),dtype=ag.Prosumer)
+        self.maxperiod = maxperiod
+        for i in range(N):
+            self.prosumers[i] = ag.Prosumer(maxperiod, initialprob)   
+        #self.bgmax = np.zeros((N,maxperiod))
+        
+        self.LCostmax = np.zeros(maxperiod)
+        self.LCostmin = np.zeros(maxperiod)
+        
+        self.insg = np.zeros(maxperiod)       
+        self.outsg = np.zeros(maxperiod)
+        
+        self.ValEgoc = np.zeros(maxperiod)
+        self.ValNoSG = np.zeros(maxperiod)
+        self.ValSG = np.zeros(maxperiod)
+        self.Reduct = np.zeros(maxperiod)
+        dt = np.dtype([('agent', np.int), ('strategy', ag.Mode)])
+        self.strategy_profile = np.ndarray(shape=(N, maxperiod), dtype=dt)
+        self.Cost = np.zeros(maxperiod)
+        self.DispSG = np.zeros(maxperiod)
+        
+        
+        
+        
+        
