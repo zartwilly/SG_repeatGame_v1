02da6ac12dd91f8365a62e3b899337acf68e7302
zartwilly <zartwilly@gmail.com>
2024-06-17 11:16:23 +0200

    RunAPP_TEST:REFACTOR: delete the older version of functions is names functions finishing without DBG.

--- runAppTEST.py
+++ runAppTEST.py
@@ -21,100 +21,13 @@ from pathlib import Path
 
 
 
-def config_instance(N_actors, nbperiod, rho):
-    """
-    generate data for a game with 
-    N_actors prosumers
-    nbperiod periods
-    rho next periods to predict values 
-    the shape data is N*(nbperiod+rho)
+###############################################################################
+#                DEBUT : ALGO LRI_REPART_DBG
+###############################################################################
 
-    Parameters
-    ----------
-    N_actors : int
-        number of prosumers
-    nbperiod : int
-        number of periods .
-    rho : int
-        the next periods to add at T periods. In finish, we generate (T + rho) periods.
-        This parameter enables the prediction of values from T+1 to T + rho periods
-        rho << T ie rho=3 < T=5
-
-    Returns
-    -------
-    g : TYPE
-        DESCRIPTION.
-
-    """
-    g = ig2.Instancegenaratorv2(N=N_actors, T=nbperiod, rho=rho)
-    transitionprobabilities = [0.4, 0.6, 0.5, 0.4]
-    repartition = [8, 7]
-    
-    # values = [m1a,M1a,m1b,M1b,m2b,M2b,cb,m1c,M1c,m2c,M2c,m3c,M3c,m4c,M4c]
-    values = [[5,20],[5,15,25,35,24],[20,30,35,50,20,35,40,55]]
-    
-    # probabilities = [P1b,P2b,P1c,P2c,P3c,P4c]
-    probabilities = [[0.5,0.5],[0.7,0.6,0.6,0.5]]
-    
-
-    g.generate(transitionprobabilities,repartition,values,probabilities)
-    
-    return g
-
-def Initialization_game(scenario):
-    """
-    initialization of variables of an object application 
-    
-    Returns
-    -----
-    App
-    """
-    # Load all scenario parameters
-    name = scenario["name"]
-    for var, val in scenario["algo"]["LRI_REPART"].items():
-        globals()[var] = val
-    
-    for var, val in scenario["instance"].items():
-        globals()[var] = val
-        
-    for var, val in scenario["simul"].items():
-        globals()[var] = val
-        """
-    maxstep = 5 * pow(10, 1)            #  5 * pow(10, 4)
-    maxstep_init = 5
-    slowdownfactor = pow(10, -3)        # 0.001
-    threshold = 0.8
-    N_actors = 15
-    nbPeriod = 10                      # 101
-    initialprob = 0.5
-    mu = pow(10, -1)                    # 0.1
-    rho = 5                             # 0.1
-    h = 5
-"""
-
-    # Initialisation of the apps
-    application = apps.App(N_actors=N_actors, maxstep=maxstep, mu=mu, 
-                           b=slowdownfactor, rho=rho, h=h, 
-                           maxstep_init=maxstep_init, threshold=threshold)
-    application.SG = sg.Smartgrid(N=N_actors, nbperiod=nbPeriod, 
-                                  initialprob=initialprob, rho=rho)
-    
-    # Configuration of the instance generator
-    g = config_instance(N_actors=N_actors, nbperiod=nbPeriod, rho=rho)
-    
-    # Initialisation of production, consumption and storage using the instance generator
-    N = application.SG.prosumers.size
-    T = application.SG.nbperiod
-    
-    for i in range(N):
-        for t in range(T):
-            application.SG.prosumers[i].production[t] = g.production[i][t]
-            application.SG.prosumers[i].consumption[t] = g.consumption[i][t]
-        application.SG.prosumers[i].storage[0] = 0
-        application.SG.prosumers[i].smax = 20
-        
-    return application
-
+#------------------------------------------------------------------------------
+#                FIN : Save data in the log file
+#------------------------------------------------------------------------------
 def monitoring_after_algorithm(algoName, file, application):
     """
     
@@ -178,259 +91,14 @@ def monitoring_before_algorithm(file, application):
             file.write(" Consumption : " + str(application.SG.prosumers[i].consumption[t]))
             file.write(" Storage : " + str(application.SG.prosumers[i].storage[t])+ "\n")
             
+#------------------------------------------------------------------------------
+#                FIN : Save data in the log file
+#------------------------------------------------------------------------------
     
-def run_syA(scenario, logfiletxt):
-    """
-    run syA algorithm
-
-    Parameters
-    ----------
-    logfile : txt
-        path Logs file 
-    Returns
-    -------
-    None.
-
-    """
-    
-    # Initialisation of the apps
-    application = Initialization_game(scenario)
-    
-    # ignore last period to exclude overflow: I do not know the importance to exclude last period
-    # application.SG.maxperiod = application.SG.maxperiod - 1
-
-
-    # Display for the run beginning 
-    file = io.open(logfiletxt,"w")                                              # Logs file
-    
-    monitoring_before_algorithm(file, application)
-    
-    # Execute SyA
-    file.write("\n_______SyA_______"+ "\n")
-    application.runSyA(plot=False,file=file)
-    
-    monitoring_after_algorithm(algoName='syA', file=file, application=application)
-    
-    # End execute syA
-    print("________RUN END syA ",1,"_________ \n")
-    
-    return application
-
-def run_CSA(scenario, logfiletxt):
-    """
-    run CSA algorithm
-
-    Parameters
-    ----------
-    logfile : txt
-        path Logs file 
-    Returns
-    -------
-    None.
-
-    """
-    # Initialisation of the apps
-    application = Initialization_game(scenario)
-
-    # Display for the run beginning 
-    file = io.open(logfiletxt,"w")                                              # Logs file
-    
-    monitoring_before_algorithm(file, application)
-    
-    
-    # Execute CSA
-    file.write("\n_______CSA_______"+ "\n")
-    application.runCSA(plot=False, file=file)
-    
-    monitoring_after_algorithm(algoName='CSA', file=file, application=application)
-    
-    
-    
-    # End execute CSA
-    print("________RUN END CSA ",1,"_________ \n")
-    
-    return application
-
-
-def run_SSA(scenario, logfiletxt):
-    """
-    run SSA (selfish stock algorithm) algorithm
-
-    Parameters
-    ----------
-    logfile : txt
-        path Logs file 
-    Returns
-    -------
-    None.
-
-    """
-    # Initialisation of the apps
-    application = Initialization_game(scenario)
-    
-    # ignore last period to exclude overflow: I do not know the importance to exclude last period
-    # application.SG.maxperiod = application.SG.maxperiod - 1
-
-
-    # Display for the run beginning 
-    file = io.open(logfiletxt,"w")                                              # Logs file
-    
-    monitoring_before_algorithm(file, application)
-    
-    
-    # Execute SSA
-    file.write("\n_______SSA_______"+ "\n")
-    application.runSSA(plot=False,file=file)
-    
-    monitoring_after_algorithm(algoName='SSA', file=file, application=application)
-    
-    
-    # End execute syA
-    print("________RUN END SSA ",1,"_________ \n")
-    
-    return application
-
-def run_LRI_REPART(scenario, logfiletxt):
-    """
-    run LRI REPART algorithm
-
-    Parameters
-    ----------
-    logfile : txt
-        path Logs file 
-    Returns
-    -------
-    None.
-
-    """
-        
-    # Initialisation of the apps
-    application = Initialization_game(scenario)
-
-    # Display for the run beginning 
-    file = io.open(logfiletxt,"w")                                              # Logs file
-    
-    monitoring_before_algorithm(file, application)
-    
-    
-    # # Execute LRI_REPART
-    algoName = "LRI_REPART"
-    file.write(f"\n_______{algoName}_______"+ "\n")
-    #application.runLRI_REPART(plot=False, file=file)
-    application.runLRI_REPART_SAVERunning(plot=False, file=file)
-    
-    monitoring_after_algorithm(algoName=algoName, file=file, application=application)
-    
-    
-    
-    # End execute LRI REPART
-    print("________RUN END LRI_REPART ",1,"_________ \n")
-    
-    return application
-
-
-###############################################################################
-#                DEBUT : ALGO LRI_REPART_DBG
-###############################################################################
-
 #------------------------------------------------------------------------------
 #                DEBUT : Generer des donnees selon scenarios
 #------------------------------------------------------------------------------
 
-def creation_instance(transitionprobabilities, repartition, values, probabilities):
-    """
-    creation instance de generation de donnees
-    creation of instances generating data
-    
-    transitionprobabilities: [w, x, y, z],
-    repartition: [xx, yy]
-    # values = [m1a,M1a,m1b,M1b,m2b,M2b,cb,m1c,M1c,m2c,M2c,m3c,M3c,m4c,M4c]
-    values = [[5,20],[5,15,25,35,24],[20,30,35,50,20,35,40,55]]
-    
-    # probabilities = [P1b,P2b,P1c,P2c,P3c,P4c]
-    probabilities = [[0.5,0.5],[0.7,0.6,0.6,0.5]]
-
-    Returns
-    -------
-    None.
-
-    """
-    
-    scenario = {"transitionprobabilities": transitionprobabilities, 
-                      "repartition": repartition,
-                      "values": values,
-                      "probabilities": probabilities,
-                      "scenarioName": "scenarioSelfish"
-                      }
-        
-    pass
-
-def generer_data_from_scenario(newInstance:bool, scenario:dict, N_actors:int, 
-                               nbperiod:int, rho:int):
-    """
-    generate data from new/existing scenario
-
-    Parameters
-    ----------
-    newInstance : bool
-        DESCRIPTION.
-    scenario : dict
-        DESCRIPTION.
-        exple: {"transitionprobabilities": transitionprobabilities, 
-                "repartition": repartition,
-                "values": values,
-                "probabilities": probabilities,
-                "scenarioName": "scenarioSelfish"
-                }
-    N_actors : int
-        DESCRIPTION.
-    nbperiod : int
-        DESCRIPTION.
-    rho : int
-        DESCRIPTION.
-
-    Returns
-    -------
-    None.
-
-    """
-    transitionprobabilities = [0.4, 0.6, 0.5, 0.4]
-    repartition = [8, 7]
-    # values = [m1a,M1a,m1b,M1b,m2b,M2b,cb,m1c,M1c,m2c,M2c,m3c,M3c,m4c,M4c]
-    values = [[5,20],[5,15,25,35,24],[20,30,35,50,20,35,40,55]]
-    # probabilities = [P1b,P2b,P1c,P2c,P3c,P4c]
-    probabilities = [[0.5,0.5],[0.7,0.6,0.6,0.5]]
-    
-    scenario = {"transitionprobabilities": transitionprobabilities, 
-                "repartition": repartition,
-                "values": values,
-                "probabilities": probabilities,
-                "scenarioName": "scenarioSelfish"
-                }
-    
-    if newInstance:
-        # creation instance
-        # generation de donnees
-        # serialisation
-        # sauvegarde pickle
-        
-        # creation instance
-        g = ig2.Instancegenaratorv2(N=N_actors, T=nbperiod, rho=rho)
-        
-        # generation de donnees
-        g.generate(scenario["transitionprobabilities"],
-                   scenario["repartition"], scenario["values"],
-                   scenario["probabilities"])
-        
-        
-    else:
-        # recuperation du pickle
-        # deserialisation 
-        # generation data
-        # sauvegarde
-    
-        pass
-    
 
 def generer_data_from_scenario_DBG(scenario:dict,
                                    N_actors:int, nbperiod:int, rho:int,
@@ -541,6 +209,7 @@ def Initialization_game_DBG(scenario):
         for t in range(T):
             application.SG.prosumers[i].production[t] = g.production[i][t]
             application.SG.prosumers[i].consumption[t] = g.consumption[i][t]
+        # put initial storage variable 
         application.SG.prosumers[i].storage[0] = 0
         application.SG.prosumers[i].smax = 20
         
@@ -785,6 +454,7 @@ if __name__ == '__main__':
     scenarioPath = "./scenario1.json"
     scenarioPath = "./data_scenario/scenario_test_LRI.json"
     scenarioPath = "./data_scenario/scenario_SelfishDebug_LRI_N4_T3.json"
+    scenarioPath = "./data_scenario/scenario_SelfishDebug_LRI_N10_T5.json"
     
     
     import time
